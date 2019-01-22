65d9a57bd31efa60a58a123d23fcefc7b2397a50
ssmilin <38403587+ssmilin@users.noreply.github.com>
2019-01-22 22:29:24 +0530

    fix icon issue

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -15,10 +15,14 @@ class SidePanel extends Component {
         <div className="logo">
           <a href="#">
             <div className="logo-img">
-              <img src={logo} alt="logo_image" height="80" width="80" />
+              <img
+                src="https://uploads.codesandbox.io/uploads/user/c1e05e50-0c65-4559-b17d-64e5dbc5c73b/amIz-overleaf_og_logo.png"
+                alt="logo_image"
+                height="80"
+                width="80"
+              />
             </div>
           </a>
-
           <span className="log-name">7Miracle</span>
         </div>
 
@@ -26,40 +30,31 @@ class SidePanel extends Component {
           <ul className="nav">
             <li>
               <NavLink to="/">
-                <i className="pe-7s-graph" />
-
+                <i className="fa fa-pie-chart" />
                 <p>Dashboard</p>
               </NavLink>
             </li>
-
             <li>
               <NavLink to="/user">
-                <i className="pe-7s-add-user" />
-
+                <i className="far fa-user-plus" />
                 <p>Add Users</p>
               </NavLink>
             </li>
-
             <li>
               <NavLink to="/profile">
-                <i className="pe-7s-user" />
-
+                <i className="fas fa-user" />
                 <p>User Profile</p>
               </NavLink>
             </li>
-
             <li>
               <NavLink to="/report">
-                <i className="pe-7s-note2" />
-
+                <i className="fas fa-clipboard-list" />
                 <p>Mothly Report</p>
               </NavLink>
             </li>
-
             <li>
               <NavLink to="/contact">
-                <i className="pe-7s-call" />
-
+                <i className="fas fa-phone" />
                 <p>Contact</p>
               </NavLink>
             </li>
